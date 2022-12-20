5de4efe4d63fbb30cf2de3fb0c4373a260df574d
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-20 21:17:28 +0500

    In this C++ program I displayed Donkey  with color using C out and system statement.

---
+++ donkeycolor.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+system ("color 57");
+cout <<"-----------------------"<<endl;
+cout <<"    o   ^__^            "<<endl;
+cout <<"     o  (oo)\\______    "<<endl;
+cout <<"        (__)\\      )\\/\\"<<endl;
+cout <<"            ||----w |    "<<endl;
+cout <<"            ||     ||    "<<endl; 
+}         
