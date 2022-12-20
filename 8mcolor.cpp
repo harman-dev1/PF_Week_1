f7cb870ee60c9c9ead3dafd782b97c60240549c1
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-20 21:12:59 +0500

    In this C++ program I displayed Disk management system word with color using C out and system statement.

---
+++ 8mcolor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+
+system("color 45");
+cout <<"88888888888888888888888888888888888"<<endl;
+cout <<"8                                 8"<<endl;
+cout <<"8      Univeristy Admission       8"<<endl;
+cout <<"8       Mangement System          8"<<endl;
+cout <<"8                                 8"<<endl;
+cout <<"8                                 8"<<endl;
+cout <<"88888888888888888888888888888888888"<<endl;
+}
